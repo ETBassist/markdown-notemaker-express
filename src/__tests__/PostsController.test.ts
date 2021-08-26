914bb660e3da11d9909ea87c4d7f44a374d58548
Eugene Theriault <hybridbassist@gmail.com>
2021-08-25 21:11:54 -0700

    Update test for PostsController and add to server.ts

--- src/__tests__/PostsController.test.ts
+++ src/__tests__/PostsController.test.ts
@@ -1,27 +1,33 @@
 import request, { Response } from 'supertest';
 import { App } from '../app';
 import { PostsController } from '../controllers';
-//import knex from 'knex'
+import { db } from '../db'
 
 describe('PostsController', () => {
-//  beforeAll(() => {
-//    knex('users').insert({
-//      id: 1,
-//      firstName: 'Test',
-//      lastName: 'User'
-//    })
-//
-//    knex('posts').insert({
-//      id: 1,
-//      user_id: 1,
-//      content: 'Some post content'
-//    })
-//  })
-//
-//  afterAll(() => {
-//    knex('users').select().del();
-//    knex('posts').select().del();
-//  })
+  const database = db();
+  beforeAll( async () => {
+    await database('users').insert({
+      id: 1,
+      firstName: 'Test',
+      lastName: 'User'
+    })
+
+    await database('posts').insert({
+      id: 1,
+      user_id: 1,
+      content: 'Some post content',
+      created_at: "2019-01-01T00:00:00.000Z",
+      updated_at: "2019-01-01T00:00:00.000Z"
+    })
+  })
+
+  afterAll( async () => {
+    await database('posts').del()
+    await database('users').del()
+    .then(() => {
+      database.destroy();
+    })
+  })
 
   it('should return a post by id', async () => {
     const postsController = new PostsController();
@@ -33,6 +39,12 @@ describe('PostsController', () => {
     const response: Response = await request(app).get(`/api/v1/posts/1`)
 
     expect(response.statusCode).toEqual(200)
-    expect(response.body).toEqual({id: 1})
+    expect(response.body).toEqual({
+      id: 1,
+      user_id: '1',
+      content: 'Some post content',
+      created_at: "2019-01-01T00:00:00.000Z",
+      updated_at: "2019-01-01T00:00:00.000Z"
+    })
   });
 });
